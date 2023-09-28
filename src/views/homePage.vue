76fe95a298f7d7d5d5a030c30772a3433b6ba713
SelmanKaya <selmankaya3412@gmail.com>
2023-09-28 14:50:31 +0300

    Değişiklik açıklaması

---
+++ src/views/homePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="go">GENEL</button>
+    <hr>
+    <button @click="go">Ingilizce Kursu</button>
+    <hr>
+    <button @click="go">Faturalar</button>
+    <hr>
+    <button @click="go">Alisveris</button>
+    <hr>
+    <button @click="go">Proje</button>
+
+</template>
+<script setup>
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+const go = () => {
+    router.push({ name: 'mainPage' })
+}
+</script>
