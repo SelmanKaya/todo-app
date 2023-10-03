0944b6ea47992deee9f1391b436e621b7f003f74
SelmanKaya <selmankaya3412@gmail.com>
2023-10-03 18:05:27 +0300

    Değişiklik açıklaması

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,20 +1,22 @@
 <template>
-    <button @click="go">GENEL</button>
-    <hr>
-    <button @click="go">Ingilizce Kursu</button>
-    <hr>
-    <button @click="go">Faturalar</button>
-    <hr>
-    <button @click="go">Alisveris</button>
-    <hr>
-    <button @click="go">Proje</button>
+    <h3 class=" font-semibold text-4xl mb-5">YAPILACAK TODO ICIN BASLIK SECINIZ</h3>
+    <button @click="go('Genel')" class=" bg-green-500 hover:bg-green-600 text-2xl text-white rounded-lg  px-8  h-8 m-2  transition-colors ">Genel</button>
+    <br>
+    <button @click="go('Ingilizce')" class=" bg-green-500 hover:bg-green-600 text-2xl text-white rounded-lg  px-8  h-8 m-2 transition-colors ">Ingilizce Kursu</button>
+    <br>
+    <button @click="go('Faturalar')" class=" bg-green-500 hover:bg-green-600 text-2xl text-white rounded-lg  px-8  h-8 m-2 transition-colors ">Faturalar</button>
+    <br>
+    <button @click="go('Alisveris')" class=" bg-green-500 hover:bg-green-600 text-2xl text-white rounded-lg  px-8  h-8 m-2 transition-colors ">Alisveris</button>
+    <br>
+    <button @click="go('Proje')" class=" bg-green-500 hover:bg-green-600 text-2xl text-white rounded-lg  px-8  h-8 m-2 transition-colors ">Proje</button>
 
 </template>
 <script setup>
 import { useRouter } from "vue-router"
 
 const router = useRouter()
-const go = () => {
-    router.push({ name: 'mainPage' })
+const go = (butonAdi) => {
+    router.push({ name: 'mainPage', params: { butonAdi } })
 }
+console.log('router :>> ', router);
 </script>
